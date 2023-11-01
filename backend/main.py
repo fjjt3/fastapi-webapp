c43bd4e0756e4f400e791bea66b63ad2c0793d18
Francisco Javier Jimenez <>
2023-11-01 12:28:32 +0100

    added dependency injection to get a db session and hashing password

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
 from core.config import settings
-from db.session import engine
-from db.base import Base
+# from db.session import engine
+# from db.base import Base //  we use alembic
 
-def create_tables():
-    Base.metadata.create_all(bind=engine)
+''' def create_tables():
+    Base.metadata.create_all(bind=engine) '''
     
 def start_application():
     app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_TITLE)
-    create_tables()
+    # create_tables() we use alembic
     return app
 
 app = start_application()
